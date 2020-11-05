3a5a833b048846a34fdbd01ebdcafbbd00ab3970
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-11-05 14:06:50 +0100

    Created bin/template.py

---
+++ bin/maccharger.py
@@ -0,0 +1,19 @@
+"""maccharger
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2020-11-5"
+
+class maccharger:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(maccharger.maccharger())
+	
+	def maccharger():
+		"""maccharger first funtion
+		"""
+		return "maccharger" #for the test (see test_maccharger.py)
+		
+if __name__ == "__main__":
+	maccharger()
